6d547ef0b77027a3309415640d31d7898b9d141a
p_arkk <p_arkk@ai-dapps.com>
2021-05-04 00:07:55 +0900

    상품 상세페이지

--- src/main/index.js
+++ src/main/index.js
@@ -1,58 +1,63 @@
-import React from 'react';
-import './index.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./index.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
-function MainPage (){
-    const [products, setProducts] = React.useState([]);
-    React.useEffect(function(){
-        axios.get("https://baf10ec8-28c4-4f2d-a89e-3e3aa970272b.mock.pstmn.io/products")
-        .then(function(result){
-            const products = result.data.products;
-            setProducts(products);
-        }).catch(function(error){
-            console.error("에러 발생 : ", error);
-        });
-    }, [])
+dayjs.extend(relativeTime);
 
-    return (
-        <div>
-            <div id="banner">
-                <img src="images/banners/banner1.png" />
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get("http://localhost:8080/products")
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" />
+      </div>
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/products/${product.id}`}>
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-footer">
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                    <span className="product-date">
+                      {dayjs(product.createdAt).fromNow()}
+                    </span>
+                  </div>
+                </div>
+              </Link>
             </div>
-            <h1>판매되는 상품들</h1>
-            <div id="product-list">
-                {
-                    products.map(function(product, index){
-                        return (
-                            <div className="product-card">
-                                <Link className="product-link" to={`/products/${product.id}`}>
-                                    <div>
-                                        <img
-                                            className="product-img"
-                                            src={product.imageUrl}
-                                        />
-                                    </div>
-                                    <div className="product-contents">
-                                        <span className="product-name">
-                                            {product.name}
-                                        </span>
-                                        <span className="product-price">
-                                            {product.price}원
-                                        </span>
-                                        <div className="product-seller">
-                                            <img className="product-avatar" src="images/icons/avatar.png" />
-                                            <span>{product.seller}</span>
-                                        </div>
-                                    </div>
-                            </Link>
-                        </div>
-                        );
-                    })
-                }
-            </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
--- src/product/index.js
+++ src/product/index.js
@@ -1,42 +1,44 @@
-import { useParams } from 'react-router-dom';
-import './index.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import "./index.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function ProductPage(){
-    const { id } = useParams();
-    const [product, setProduct] = useState(null);
-    useEffect(function(){
-        axios.get(`https://baf10ec8-28c4-4f2d-a89e-3e3aa970272b.mock.pstmn.io/products/${id}`)
-        .then(function(result){
-            console.log(result);
-        setProduct(result.data);
-        })
-        .catch(function(error){
+function ProductPage() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(`http://localhost:8080/products/${id}`)
+      .then(function (result) {
+        console.log(result);
+        setProduct(result.data.product);
+      })
+      .catch(function (error) {
         console.error(error);
-        });
-    },[]);
+      });
+  }, []);
 
-    if(product === null){
-        return <h1>상품 정보를 받고 있습니다...</h1>;
-    }
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
 
-    return (
-        <div>
-            <div id="image-box">
-                <img src={"/" + product.imageUrl} />
-            </div>
-            <div id="profile-box">
-                <img src="/images/icons/avatar.png" />
-                <span>{product.seller}</span>
-            </div>
-            <div id="content-box">
-                <div id="name">{product.name}</div>
-                <div id="price">{product.price}</div>
-                <div id="description">{product.description}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="content-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}</div>
+        <div id="createdAt">{product.createdAt}</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
